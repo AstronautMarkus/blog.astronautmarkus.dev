9f52c79efa2a90037fe355bf64145bbc8577a03c
Marcos Reyes <astronautmarkus@gmail.com>
2025-12-01 19:17:18 -0300

    feat: include tags in post retrieval responses

--- backend/api.blog.astronautmarkus.dev/app/routes/posts/get_posts.py
+++ backend/api.blog.astronautmarkus.dev/app/routes/posts/get_posts.py
@@ -5,16 +5,20 @@ from app.models import Post, PostTag, Tag
 @posts_bp.route('/', methods=['GET'])
 def get_posts():
     posts = Post.query.all()
-    posts_data = [
-        {
+    posts_data = []
+    for post in posts:
+        post_tags = PostTag.query.filter_by(post_id=post.id).all()
+        tag_ids = [pt.tag_id for pt in post_tags]
+        tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+        tags_names = [tag.name for tag in tags]
+        posts_data.append({
             'id': post.id,
             'title': post.title,
             'description': post.description,
             'image_url': post.image_url,
-            'slug': post.slug
-        }
-        for post in posts
-    ]
+            'slug': post.slug,
+            'tags': tags_names
+        })
     return jsonify(posts_data)
 
 @posts_bp.route('/<string:slug>', methods=['GET'])
@@ -24,11 +28,18 @@ def get_post_by_slug(slug):
     if not post:
         return jsonify({'error': 'Post not found'}), 404
 
+    post_id = post.id
+    post_tags = PostTag.query.filter_by(post_id=post_id).all()
+    tag_ids = [pt.tag_id for pt in post_tags]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+    tags_names = [tag.name for tag in tags]
+
     post_data = {
         'id': post.id,
         'title': post.title,
         'description': post.description,
         'image_url': post.image_url,
-        'slug': post.slug
+        'slug': post.slug,
+        'tags': tags_names
     }
     return jsonify(post_data)
