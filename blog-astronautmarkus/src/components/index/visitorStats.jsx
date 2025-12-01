8a395ead75120508059ed07b4c1202502dc218c4
Marcos Reyes <astronautmarkus@gmail.com>
2025-12-01 00:19:34 -0300

    feat: add VisitorStats component to display visitor statistics on the homepage WIP

---
+++ blog-astronautmarkus/src/components/index/visitorStats.jsx
@@ -0,0 +1,13 @@
+function VisitorStats() {
+  return (
+    <div class="bg-[#c5c1c0] shadow p-4">
+        <h3 class="text-lg font-semibold flex items-center gap-2 mb-4 playstation-fonts md:text-xl text-gray-900">
+            <i class="hn hn-globe-americas-solid"></i>Visitors Stats
+        </h3>
+      <p class="text-lg">Total Visitors: <strong>1.234</strong></p>
+      <p class="text-lg">Page Views: <strong>5.678</strong></p>
+    </div>
+  );
+}
+
+export default VisitorStats;
